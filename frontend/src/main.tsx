d0568ff08d6ae15ae8165846fe2329ee3bc27b02
timspizza <peisen.jiang2001@gmail.com>
2025-04-08 19:27:09 -0600

    fix language detector problem

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "@/styles/tailwind.output.css";
 import '@/index.css';
+import '@/i18n/config'; // Import and initialize i18n
 import App from './App.tsx';
-import './i18n/config'; // Import and initialize i18n
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
